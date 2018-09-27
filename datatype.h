68e062bc6423655059dfde5c3ea724f29c4ffdbc
caihuaiyu <caihuaiyu123@gmail.com>
2018-09-28 00:01:02 +0800

    add datatye.h

---
+++ datatype.h
@@ -0,0 +1,13 @@
+
+#if !defined(_DATATYPE_H_)
+#define _DATATYPE_H_
+
+#define uint8 unsigned char
+#define int8 char
+#define uint16 unsigned short
+#define int16 short
+#define uint32 unsigned int
+#define int32 int
+
+
+#endif // _DATATYPE_H_
